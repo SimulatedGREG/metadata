012372cd9d4526863baeb52d95aeba98c2b50e62
SimulatedGREG <simulatedgreg@gmail.com>
2016-08-10 04:45:30 -0500

    create diff data to separated computed metadata in editor

--- app/src/components/MainView/Editor.vue
+++ app/src/components/MainView/Editor.vue
@@ -50,11 +50,7 @@
       padding: 10px;
       width: 100%;
 
-      &:focus {
-        border: solid 1px rgba(#0e7ac4, .5);
-        box-shadow: 2px 2px 2px .5px rgba(#000, .05);
-      }
-
+      &:focus,
       &:hover {
         border: solid 1px rgba(#0e7ac4, .5);
         box-shadow: 2px 2px 2px .5px rgba(#000, .05);
@@ -65,35 +61,35 @@
 
 <template>
   <div class="editor">
-    <p class="song-filename">MyCoolSong.mp3</p>
+    <p class="song-filename">{{ editorMetadata.filename }}</p>
     <div class="left">
       <div class="input-group">
         <label for="song-title">Title</label>
-        <input id="song-title" v-model="editorMetadata.title" type="text">
+        <input id="song-title" v-model="diff.title" :value="editorMetadata.title" type="text">
       </div>
       <div class="input-group">
         <label for="song-artist">Artist</label>
-        <input id="song-artist" v-model="editorMetadata.artist" type="text">
+        <input id="song-artist" v-model="diff.artist" :value="editorMetadata.artist" type="text">
       </div>
       <div class="input-group">
         <label for="song-album-artist">Album Artist</label>
-        <input id="song-album-artist" v-model="editorMetadata.album_artist" type="text">
+        <input id="song-album-artist" v-model="diff.album_artist" :value="editorMetadata.album_artist" type="text">
       </div>
       <div class="input-group">
         <label for="song-album">Album</label>
-        <input id="song-album" v-model="editorMetadata.album" type="text">
+        <input id="song-album" v-model="diff.album" :value="editorMetadata.album" type="text">
       </div>
       <div class="input-group">
         <label for="song-date">Year</label>
-        <input id="song-date" v-model="editorMetadata.date" type="text">
+        <input id="song-date" v-model="diff.date" :value="editorMetadata.date" type="text">
       </div>
       <div class="input-group">
         <label for="song-genre">Genre</label>
-        <input id="song-genre" v-model="editorMetadata.genre" type="text">
+        <input id="song-genre" v-model="diff.genre" :value="editorMetadata.genre" type="text">
       </div>
       <div class="input-group">
         <label for="song-track">Track #</label>
-        <input id="song-track" v-model="editorMetadata.track" type="text">
+        <input id="song-track" v-model="diff.track" :value="editorMetadata.track" type="text">
       </div>
     </div>
     <div class="right">
@@ -105,6 +101,7 @@
 
 <script>
   import {
+    filePaths,
     metadata,
     selectedPaths
   } from 'src/vuex/getters'
@@ -138,19 +135,38 @@
           blob[fields[i]] = this.parseField(fields[i])
         }
 
-        console.log(blob)
+        if (this.selectedPaths.length === 1) {
+          let split = this.filePaths[this.selectedPaths[0]].split('/')
+          blob.filename = split[split.length - 1]
+        } else blob.filename = 'Multiple Filenames'
 
         return blob
+      }
+    },
+    data () {
+      return {
+        diff: {
+          title: '',
+          artist: '',
+          album_artist: '',
+          album: '',
+          date: '',
+          genre: '',
+          track: ''
+        }
       }
     },
     methods: {
       parseField (field) {
         let f = uniq(map(this.currentMetadata, field))
-        return f.length === 1 ? f[0] : 'Multiple Values'
+
+        if (!f[0]) return 'Not Specified'
+        else return f.length === 1 ? f[0] : 'Multiple Values'
       }
     },
     vuex: {
       getters: {
+        filePaths,
         metadata,
         selectedPaths
       }
